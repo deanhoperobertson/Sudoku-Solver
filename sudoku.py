28085994fefb717fda51245eb9ffbc5ada101108
Dean Hope Robertson <deanhoperobertson@gmail.com>
2021-05-12 10:18:07 +0100

    Show image

--- sudoku.py
+++ sudoku.py
@@ -1,2 +1,16 @@
 import os
 import cv2
+from PIL import Image
+
+PATH = os.getcwd()+"/Images/Easy.jpg"
+
+def show_image():
+	#Image.open(PATH).show()
+
+	image = cv2.imread(PATH)
+	cv2.imshow("Window", image)
+	cv2.waitKey(2000)
+	cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+	show_image()
